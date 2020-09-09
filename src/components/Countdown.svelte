a12c840ba6f2594b89e1266ae1096ba7b5f9817a
Brad Garropy <bradgarropy@gmail.com>
2020-09-08 21:42:51 -0500

    progress

--- src/components/Countdown.svelte
+++ src/components/Countdown.svelte
@@ -2,7 +2,7 @@
     export let onCompleted
 
     let id
-    let countdown = 3
+    const countdown = 3
 
     id = setInterval(() => {
         if (countdown === 1) {
@@ -11,11 +11,31 @@
             return
         }
 
-        countdown = countdown - 1
+        // countdown = countdown - 1
     }, 1000)
 
     const audio = document.getElementById("countdown")
     audio.play()
 </script>
 
-<p class="text-center font-body text-black text-7xl">{countdown}</p>
+<!-- <p class="text-center font-body text-black text-7xl">{countdown}</p> -->
+
+<div class="flex flex-row justify-center font-body font-bold gap-32">
+    <div
+        class="flex items-center justify-center w-48 h-48 text-white
+            rounded-full border-solid border-white border-8 text-7xl bg-green">
+        3
+    </div>
+
+    <div
+        class="flex items-center justify-center w-48 h-48 -mx-40 text-white
+            rounded-full border-solid border-white border-8 text-7xl bg-yellow">
+        2
+    </div>
+
+    <div
+        class="flex items-center justify-center w-48 h-48 text-white
+            rounded-full border-solid border-white border-8 text-7xl bg-red">
+        1
+    </div>
+</div>
